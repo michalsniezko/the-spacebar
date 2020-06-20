5c3948bfc5194f5b351334b63fa15ca0cd6a0448
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-20 19:24:10 +0200

    after the faker lesson

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -7,13 +7,30 @@ use Doctrine\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixtures
 {
+    private static $articleTitles = [
+        'Why Asteroids Taste Like Bacon',
+        'Life on Planet Mercury: Tan, Relaxing and Fabulous',
+        'Light Speed Travel: Fountain of Youth or Fallacy',
+    ];
+
+    private static $articleImages = [
+        'asteroid.jpeg',
+        'mercury.jpeg',
+        'lightspeed.png',
+    ];
+
+    private static $articleAuthors = [
+        'Mike Ferengi',
+        'Amy Oort',
+    ];
+
     protected function loadData(ObjectManager $manager)
     {
         $this->createMany(Article::class, 10, function (Article $article, $count) {
 
             $article
-                ->setTitle('Why Asteroids Taste Like Bacon')
-                ->setSlug('why-asteroids-taste-like-bacon' . $count)
+                ->setTitle($this->faker->randomElement(self::$articleTitles))
+                ->setSlug($this->faker->slug)
                 ->setContent(
                     <<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident beef ribs aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow turkey shank eu pork belly meatball non cupim.
@@ -24,13 +41,13 @@ Do mollit deserunt prosciutto laborum. Duis sint tongue quis nisi. Capicola qui 
 EOF
                 );
 
-            if (rand(1, 10) > 2) {
-                $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+            if ($this->faker->boolean(70)) {
+                $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
 
-            $article->setAuthor('Mike Ferengi')
-                ->setHeartCount(rand(5, 100))
-                ->setImageFilename('asteroid.jpeg');
+            $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
+                ->setHeartCount($this->faker->numberBetween(5, 100))
+                ->setImageFilename($this->faker->randomElement(self::$articleImages));
         });
 
         $manager->flush();
