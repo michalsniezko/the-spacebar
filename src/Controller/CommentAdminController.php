057c2f169c9b417e4049d3c818ca85a9d20fef10
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-21 17:34:27 +0200

    added the search functionality

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,17 +4,24 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommentAdminController extends AbstractController
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @param CommentRepository $repository
+     * @param Request $request
+     * @return Response
      */
-    public function index(CommentRepository $repository)
+    public function index(CommentRepository $repository, Request $request)
     {
+        $q = $request->query->get('q');
 
-        $comments = $repository->findBy([], ['createdAt' => 'DESC']);
+        $comments = $repository->findAllWithSearch($q);
+
         return $this->render('comment_admin/index.html.twig', [
             'comments' => $comments,
         ]);
