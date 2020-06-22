fe66321afe0ae61edc7607b947ea1d55515e983a
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-22 08:32:44 +0200

    at the beginning of the symfony security course

--- src/DataFixtures/TagFixture.php
+++ src/DataFixtures/TagFixture.php
@@ -5,12 +5,15 @@ namespace App\DataFixtures;
 use App\Entity\Tag;
 use Doctrine\Persistence\ObjectManager;
 
-class TagFixtures extends BaseFixtures
+class TagFixture extends BaseFixture
 {
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(Tag::class, 10, function(Tag $tag) {
+        $this->createMany(10, 'main_tags', function() {
+            $tag = new Tag();
             $tag->setName($this->faker->realText(20));
+
+            return $tag;
         });
 
         $manager->flush();
