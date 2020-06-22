ec16b94f4edbbc15837e59d4e08fe243f639c362
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-22 11:03:29 +0200

    implemented access_control

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
