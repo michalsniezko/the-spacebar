87dd3130d48cb28ca6a85a2a6bb00b15fcd8efbe
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-22 13:46:30 +0200

    implemented api token authentication system

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ApiToken;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -18,7 +19,7 @@ class UserFixture extends BaseFixture
 
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(10, 'main_users', function ($i) {
+        $this->createMany(10, 'main_users', function ($i) use ($manager) {
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
@@ -29,6 +30,12 @@ class UserFixture extends BaseFixture
             $user->setPassword(
                 $this->passwordEncoder->encodePassword($user, 'engage')
             );
+
+            $apiToken1 = new ApiToken($user);
+            $apiToken2 = new ApiToken($user);
+
+            $manager->persist($apiToken1);
+            $manager->persist($apiToken2);
 
             return $user;
         });
