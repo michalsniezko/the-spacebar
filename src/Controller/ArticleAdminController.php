f72d228df14626b7adf656610a5190a5a6615576
Michał Śnieżko <michalsniezko@gmail.com>
2020-06-22 12:28:14 +0200

    role hierarchy

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_ADMIN_ARTICLE")
+ */
 class ArticleAdminController extends AbstractController
 {
     /**
--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_ADMIN_COMMENT")
+ */
 class CommentAdminController extends AbstractController
 {
     /**
@@ -22,15 +25,12 @@ class CommentAdminController extends AbstractController
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
         $q = $request->query->get('q');
-
         $queryBuilder = $repository->getAllWithSearchQueryBuilder($q);
-
         $pagination = $paginator->paginate(
             $queryBuilder,
             $request->query->getInt('page', 1),
             10
         );
-
 
         return $this->render('comment_admin/index.html.twig', [
             'pagination' => $pagination,
